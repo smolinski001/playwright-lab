236193bbe966925cf005e1b3d5b0446416b1f937
Smolinski01 <smolinski001@gmail.com>
2026-03-19 14:27:15 +0100

    test(api): add GET single ID test

--- tests/api/booking.spec.ts
+++ tests/api/booking.spec.ts
@@ -8,3 +8,11 @@ test("Testing API", async ({ request }) => {
   expect(Array.isArray(body)).toBe(true);
   expect(body.length).toBeGreaterThan(0);
 });
+
+test("single ID", async ({ request }) => {
+  const response = await request.get("https://restful-booker.herokuapp.com/booking/1");
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+  expect(body.firstname).toBeDefined();
+});
