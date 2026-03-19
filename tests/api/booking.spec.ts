581a92f248fb5010f87f7504c51a6347436a20bb
Smolinski01 <smolinski001@gmail.com>
2026-03-19 15:34:55 +0100

    test(api): add POST auth token test

--- tests/api/booking.spec.ts
+++ tests/api/booking.spec.ts
@@ -16,3 +16,13 @@ test("single ID", async ({ request }) => {
   const body = await response.json();
   expect(body.firstname).toBeDefined();
 });
+
+test("Testing POST", async ({ request }) => {
+  const response = await request.post("https://restful-booker.herokuapp.com/auth", {
+    data: { username: "admin", password: "password123" },
+  });
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+  expect(body.token).toBeDefined();
+});
