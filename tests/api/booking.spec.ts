c934f6f9bba5d3ee3e40dd608fb78b4e82d9f136
Smolinski01 <smolinski001@gmail.com>
2026-03-19 13:36:42 +0100

    test(api): add GET all bookings test with status and body validation

---
+++ tests/api/booking.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test("Testing API", async ({ request }) => {
+  const response = await request.get("https://restful-booker.herokuapp.com/booking");
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+  expect(Array.isArray(body)).toBe(true);
+  expect(body.length).toBeGreaterThan(0);
+});
